87e7b511c9c39e0b6cdff09216b9132a594ef278
SkyPuncher <Djolecekic@gmail.com>
2020-12-30 07:25:23 +0100

    Cetvrti commit. Knjige dostupne da se edituju kroz admin panel. Home page prikazuje naslove knjiga

--- mysite/bookstore/views.py
+++ mysite/bookstore/views.py
@@ -6,7 +6,6 @@ from django.template import loader
 
 def index(request):
     latest_books_list = Book.objects.order_by('-id')[:]
-    output = ', '.join([b.title for b in latest_books_list])
     context = {
         'latest_books_list': latest_books_list,
     }
@@ -15,7 +14,8 @@ def index(request):
 
 def detail(request, book_id):
     book = Book.objects.get(pk=book_id)
-    return HttpResponse(book.author)
+    context = {'book': book}
+    return render(request, 'bookstore/details.html', context)
 
 
 def broj(request, br=0):

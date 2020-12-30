c988a15f924dd9ad8a77018e2773112726540dd7
SkyPuncher <Djolecekic@gmail.com>
2020-12-30 01:31:54 +0100

    Treci commit. Napravljena baza

--- mysite/bookstore/models.py
+++ mysite/bookstore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    category = models.CharField(max_length=64, default='None')
+    book_cover = models.TextField(default='No image available')
+    price = models.IntegerField()
+    description = models.TextField()
